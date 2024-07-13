5422ae4080604bce8525c28d43ba3e516da382d9
binary-digits-inc <brianthedeveloper2@gmail.com>
2024-07-13 17:33:08 +0300

    first commit

---
+++ Full-Stack-E-Commerce-MERN-APP/frontend/src/helpers/productCategory.js
@@ -0,0 +1,12 @@
+const productCategory = [
+    { id : 1, label : "Laptops", value : "laptops"},
+    { id : 2, label : "Desktop Computers", value : "desktop"},
+    { id : 3, label : "Phones", value : "phones"},
+    { id : 4, label : "Printers", value : "printers"},
+    { id : 5, label : "Speakers", value : "speakers"},
+    { id : 6, label : "Accesories", value : "accesories"},
+    
+]
+
+
+export default productCategory
